053a8584aca98e8b1fc4c83c6f80d3c1700ac111
ywonchae1 <ywonchae62@gmail.com>
2023-05-12 07:31:33 +0900

    #3 mod: 경로 변경

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/", indexRouter);
-app.use("/user", residentRouter);
+app.use("/resident", residentRouter);
 
 //공인중개사 페이지 접근
 app.use('/agent', agentRouter);
