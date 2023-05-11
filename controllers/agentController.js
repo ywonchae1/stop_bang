d71438233efd4ef5e7b519c59292a3a2bdcb9b9f
ywonchae1 <ywonchae62@gmail.com>
2023-05-11 22:04:39 +0900

    fix : 누락된 ejs 변수 추가

--- controllers/agentController.js
+++ controllers/agentController.js
@@ -7,7 +7,7 @@ module.exports = {
 			console.log(agentReviews);
 			cmpName = agentReviews[0].cmp_nm;
 			raRegno = agentReviews[0].ra_regno;
-			res.render('agent/agentIndex.ejs', {title: `${cmpName}의 후기`, reviewData: agentReviews, direction: `/review/${cmpName}/create`, raRegno: raRegno});
+			res.render('agent/agentIndex.ejs', {title: `${cmpName}의 후기`, agentReviewData: agentReviews, direction: `/review/${cmpName}/create`, raRegno: raRegno});
 		});
 	}
 };
