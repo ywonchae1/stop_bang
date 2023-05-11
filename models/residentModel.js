aa44b2105b0fdbd90d9d1350ecb428971491e44b
ywonchae1 <ywonchae62@gmail.com>
2023-05-12 07:48:38 +0900

    #3 mod: 쿼리 결과 r_username 추가

--- models/residentModel.js
+++ models/residentModel.js
@@ -4,11 +4,14 @@ let residentModel = {
   getReviewById: async (id, result) => {
     try {
       const res = await sql.query(
-        `SELECT  rv_id, cmp_nm, address, agentList_ra_regno, rating, content, created_time 
-          FROM review
-          INNER JOIN agentList
-          ON agentList.ra_regno = agentList_ra_regno
-          WHERE resident_r_id = ?`,
+				`SELECT rv_id, r_id, r_username, cmp_nm, address, ra_regno, rating, content, newTable.created_time
+					FROM agentList
+					JOIN (SELECT rv_id, r_id, r_username, agentList_ra_regno, rating, content, review.created_time 
+            FROM review
+            JOIN resident
+            ON r_id=resident_r_id
+            WHERE r_id=?) newTable
+					ON ra_regno=agentList_ra_regno`,
         [id]
       );
       result(res);
@@ -20,7 +23,7 @@ let residentModel = {
     try {
       const res = await sql.query(
         `SELECT review_rv_id as rv_id, cmp_nm, address, agentList_ra_regno, rating, content, opened_review.created_time AS created_time 
-          FROM opened_review 
+        FROM opened_review 
           INNER JOIN review 
           ON opened_review.review_rv_id = review.rv_id 
           INNER JOIN agentList
