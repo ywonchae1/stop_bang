d423fa6058488c3226c49d3c1705de0c701fb5ff
ywonchae62 <ywonchae62@gmail.com>
2023-05-18 23:30:08 +0900

    #12 feat: 로그인 계정 쿠키 정보로 데이터 조회

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,52 +2,53 @@
 const reviewModel = require('../models/reviewModel.js');
 
 module.exports = {
-	//후기 추가
-	createReview: (req, res) => {
-		let title = `${req.params.cmp_nm} 후기 작성하기`
-		res.render('review/writeReview.ejs', 
+    //후기 추가
+    createReview: (req, res) => {
+	reviewModel.getRealtorByRaRegno(req.params, (result) => {
+	    res.render('review/writeReview.ejs', 
 		{
-			title: title, 
-			raRegno: req.body.raRegno, 
-			cmpName: req.params.cmp_nm
+		    realtor: result
 		});
-	},
+	});
+    },
 
-	//후기 추가 DB 반영
-	creatingReview: (req, res) => {
-		console.log(req.body);
-		reviewModel.createReviewProcess(req.params, req.body, () => {
-			res.redirect(`/realtor/${req.params.cmp_nm}`);
+    //후기 추가 DB 반영
+    creatingReview: (req, res) => {
+	//쿠키로부터 로그인 계정 알아오기
+	let r_id = req.cookies.authToken;
+	if(r_id == NULL) res.send('로그인이 필요합니다.');
+	reviewModel.createReviewProcess(r_id, req.params, req.body, () => {
+	    res.redirect(`/realtor/${req.params.ra_regno}`);
+	});
+    },
+
+    //후기 수정
+    updateReview: (req, res) => {
+	reviewModel.getReviewByRvId(req.params, (residentReview) => {
+	    let cmpName = residentReview.cmp_nm;
+	    let userName = residentReview.r_username;
+	    let raRegno = residentReview.ra_regno;
+	    let rate = residentReview.rating;
+	    let description = residentReview.content;
+
+	    let title = `${cmpName} - ${userName}님의 후기 수정하기`
+	    res.render('review/updateReview.ejs', 
+		{
+		    title: title, 
+		    reviewId: req.params.rv_id, 
+		    raRegno: raRegno, 
+		    rate: rate, 
+		    description: description, 
+		    userName: userName
 		});
-	},
+	});
+    },
 
-	//후기 수정
-	updateReview: (req, res) => {
-		reviewModel.getReviewByRvId(req.params, (residentReview) => {
-			let cmpName = residentReview.cmp_nm;
-			let userName = residentReview.r_username;
-			let raRegno = residentReview.ra_regno;
-			let rate = residentReview.rating;
-			let description = residentReview.content;
-
-			let title = `${cmpName} - ${userName}님의 후기 수정하기`
-			res.render('review/updateReview.ejs', 
-			{
-				title: title, 
-				reviewId: req.params.rv_id, 
-				raRegno: raRegno, 
-				rate: rate, 
-				description: description, 
-				userName: userName
-			});
-		});
-	},
-
-	//후기 수정 DB 반영
-	updatingReview: (req, res) => {
-		reviewModel.updateReviewProcess(req.params, req.body, () => {
-			res.redirect(`/resident/myReview`);
-			//res.redirect(`/resident/${req.body.userName}/myReviews`);
-		});
-	}
+    //후기 수정 DB 반영
+    updatingReview: (req, res) => {
+	reviewModel.updateReviewProcess(req.params, req.body, () => {
+	    res.redirect(`/resident/myReview`);
+	    //res.redirect(`/resident/${req.body.userName}/myReviews`);
+	});
+    }
 };
