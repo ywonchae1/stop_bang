37c49d51a1e5b41c6a0c36e0a3f7582fe1fc5758
kcdoggo <cdoggokk@gmail.com>
2023-05-30 15:14:41 +0900

    #fix 21: get a review page

--- controllers/realtorController.js
+++ controllers/realtorController.js
@@ -8,31 +8,37 @@ module.exports = {
 	let r_id = req.cookies.authToken;
 	if(r_id === null) res.send('로그인이 필요합니다.');
 	res.locals.r_id = r_id;
-	await realtorModel.getReviewByRaRegno(req.params, r_id, (agentReviews, openedReviews, canOpen, err) => {
-	    if (agentReviews[0] === null) {
-		console.log("error occured: ", err);
+	try {
+	    let agent = await realtorModel.getRealtorProfile(req.params.ra_regno);
+	    let agentMainInfo = await realtorModel.getMainInfo(req.params.ra_regno);
+	    let agentSubInfo = await realtorModel.getEnteredAgent(req.params.ra_regno);
+	    let getReviews = await realtorModel.getReviewByRaRegno(req.params.ra_regno, r_id);
+	    let getRating = await realtorModel.getRating(req.params);
+	    res.locals.agent = agent[0];
+	    res.locals.agentMainInfo = agentMainInfo;
+	    res.locals.agentSubInfo = agentSubInfo[0][0];
+	    res.locals.agentReviewData = getReviews.reviews;
+	    res.locals.openedReviewData = getReviews.opened;
+	    res.locals.canOpen = getReviews.canOpen;
+	    res.locals.title = `${res.locals.agent.cmp_nm}의 후기`;
+	    res.locals.direction = `/review/${req.params.ra_regno}/create`;
+	    res.locals.cmpName = res.locals.agent.cmp_nm;
+	    if (getRating === null) {
+		res.locals.rating = 0;
+		res.locals.tagsData = null;
 	    } else {
-		res.locals.agentReviewData = agentReviews;
-		res.locals.openedReviewData = openedReviews;
-		res.locals.canOpen = canOpen;
-		res.locals.title = `${agentReviews[0].cmp_nm}의 후기`;
-		res.locals.direction = `/review/${req.params.ra_regno}/create`; 
-		res.locals.cmpName = agentReviews[0].cmp_nm;
+		res.locals.rating = getRating;
+		res.locals.tagsData = tags.tags;
 	    }
-	});
-	await realtorModel.getRating(req.params, (agentRating, err) => {
-	    if (agentRating[0] === null) {
-		console.log("error occured: ", err);
-	    } else {
-		res.locals.rating = agentRating;
-    res.locals.tagsData = tags.tags;
-	    }
-	});
+	} catch (err) {
+	    console.error(err.stack)
+	}
 	next();
     },
     realtorView: (req, res) => {
 	res.render('realtor/realtorIndex.ejs');
     },
+
     opening: async (req, res) => {
 	//쿠키로부터 로그인 계정 알아오기
 	let r_id = req.cookies.authToken;
@@ -41,23 +47,23 @@ module.exports = {
 	await realtorModel.insertOpenedReview(r_id, rv_id, () => {
 	    res.redirect(`/realtor/${req.params.ra_regno}`);
 	});
-},
+    },
 
-  updateBookmark: (req, res) => {
-    const r_id = req.cookies.authToken;
-    if (r_id === null) res.send("로그인이 필요합니다.");
-    else {
-      let body = {
-        raRegno: req.params.ra_regno,
-        isBookmark: req.body.bookmarkData,
-      };
-      realtorModel.updateBookmark(r_id, body, (result, err) => {
-        if (result === null) {
-          console.log("error occured: ", err);
-        } else {
-          res.redirect(`/realtor/${req.params.ra_regno}`);
-        }
-      });
-    }
-  },
+    updateBookmark: (req, res) => {
+	const r_id = req.cookies.authToken;
+	if (r_id === null) res.send("로그인이 필요합니다.");
+	else {
+	    let body = {
+		raRegno: req.params.ra_regno,
+		isBookmark: req.body.bookmarkData,
+	    };
+	    realtorModel.updateBookmark(r_id, body, (result, err) => {
+		if (result === null) {
+		    console.log("error occured: ", err);
+		} else {
+		    res.redirect(`/realtor/${req.params.ra_regno}`);
+		}
+	    });
+	}
+    },
 };
--- models/realtorModel.js
+++ models/realtorModel.js
@@ -2,72 +2,125 @@
 const db = require("../config/db.js");
 
 module.exports = {
-  getReviewByRaRegno: async (params, r_id, result) => {
-    let rawQuery = `
-	SELECT rv_id, r_username, A.rating AS rating, content, tags, cmp_nm, ra_regno, DATE_FORMAT(A.created_time,'%Y-%m-%d') AS created_time 
-	FROM agentList 
-	JOIN(SELECT rv_id, r_username, agentList_ra_regno, rating, tags, content, review.created_time AS created_time 
-	FROM resident 
-	JOIN review 
-	ON r_id=resident_r_id) A 
-	ON agentList_ra_regno=ra_regno WHERE ra_regno=?`;
-
-	let checkOpenedRawQuery = `
-	SELECT review_rv_id
-	FROM opened_review
-	WHERE resident_r_id=?`;
-	
-	let checkRPointRawQuery = `
-	SELECT r_point
-	FROM resident
-	WHERE r_id=?`;
-
-	let reviews = await db.query(rawQuery, [params.ra_regno]);
-	let opened = await db.query(checkOpenedRawQuery, [r_id]);
-	let rPoint = await db.query(checkRPointRawQuery, [r_id]);
-	let canOpen = true;
-	if(rPoint[0][0].r_point < 2) canOpen = false;
-	result(reviews[0], opened[0], canOpen);
+    getRealtorProfile: async (ra_regno) => {
+		try {
+			const res = await db.query(`
+			SELECT agentList.rdealer_nm, agentList.cmp_nm, agentList.address, agent.a_profile_image
+			FROM agentList
+			LEFT JOIN agent ON agentList.ra_regno = agent.agentList_ra_regno
+			WHERE agentList.ra_regno = ?;`,
+			[ra_regno]
+			);
+			return res[0];
+		} catch (error) {
+			return error;
+		}
     },
 
-  getRating: async (params, result) => {
-	let rawQuery = `
-	SELECT TRUNCATE(AVG(rating), 1) AS agentRating
-	FROM review
-	WHERE agentList_ra_regno=?`
-	let res = await db.query(rawQuery, [params.ra_regno]);
-	result(res[0][0]);
+    getMainInfo: async (ra_regno) => {
+		let rawQuery = `
+		SELECT ra_regno, a_image1, a_image2, a_image3, a_introduction
+		FROM agent
+		RIGHT OUTER JOIN agentList
+		ON agentList_ra_regno=ra_regno
+		WHERE ra_regno = ?;`;
+		let res = await db.query(rawQuery, [ra_regno]);
+		return res[0][0];
+    },
+
+    getEnteredAgent: async (ra_regno) => {
+		try {
+			const res = await db.query(`
+			SELECT a_office_hours, contact_number, telno, ra_regno
+			FROM agent_contact
+			RIGHT OUTER JOIN agentList
+			ON agent_contact.agent_agentList_ra_regno = agentList.ra_regno
+			LEFT OUTER JOIN agent
+			ON agentList.ra_regno = agent.agentList_ra_regno
+			WHERE ra_regno = ?;`,
+			[ra_regno]
+			);
+			return res
+		} catch (error) {
+			return error
+		}
+    },
+
+    getReviewByRaRegno: async (ra_regno, r_id) => {
+		try {
+			let rawQuery = `
+			SELECT cmp_nm, ra_regno, rv_id, r_id, r_username, rating, content, tags, DATE_FORMAT(newTable.created_time,'%Y-%m-%d') AS created_time
+			FROM agentList
+			JOIN(
+			SELECT rv_id, r_id, r_username, agentList_ra_regno, rating, tags, content, review.created_time AS created_time
+			FROM resident
+			JOIN review
+			ON r_id=resident_r_id
+			) newTable
+			ON agentList_ra_regno=ra_regno
+			WHERE ra_regno=?;`;
+
+			let checkOpenedRawQuery = `
+			SELECT review_rv_id
+			FROM opened_review
+			WHERE resident_r_id=?`;
+
+			let checkRPointRawQuery = `
+			SELECT r_point
+			FROM resident
+			WHERE r_id=?`;
+
+			let reviews = await db.query(rawQuery, [ra_regno]);
+			let opened = await db.query(checkOpenedRawQuery, [r_id]);
+			let rPoint = await db.query(checkRPointRawQuery, [r_id]);
+			let canOpen = 1;
+			if(rPoint[0][0].r_point < 2) canOpen = 0;
+			return ({reviews: reviews[0], opened: opened[0], canOpen: canOpen});
+		} catch(err) {
+			return err
+		}
+    },
+
+    getRating: async (params) => {
+		let rawQuery = `
+		SELECT TRUNCATE(AVG(rating), 1) AS agentRating
+		FROM review
+		RIGHT OUTER JOIN agentList
+		ON agentList_ra_regno=ra_regno
+		WHERE ra_regno=?;`;
+		let res = await db.query(rawQuery, [params.ra_regno]);
+		return res[0][0].agentRating;
     },
 
     insertOpenedReview: async (r_id, rv_id, result) => {
-	let insertRawQuery = `
-	INSERT
-	INTO opened_review(resident_r_id, review_rv_id)
-	VALUE(?, ?);
-	`
-	let usePointRawQuery = `
-	UPDATE resident
-	SET r_point=r_point - 2
-	WHERE r_id=?;
-	`
-	await db.query(insertRawQuery, [r_id, rv_id]);
-	await db.query(usePointRawQuery, [r_id]);
-	result();
-	},
+		let insertRawQuery = `
+		INSERT
+		INTO opened_review(resident_r_id, review_rv_id)
+		VALUE(?, ?);
+		`
+		let usePointRawQuery = `
+		UPDATE resident
+		SET r_point=r_point - 2
+		WHERE r_id=?;
+		`
+		await db.query(insertRawQuery, [r_id, rv_id]);
+		await db.query(usePointRawQuery, [r_id]);
+		result();
+    },
 
-  updateBookmark: async (id, body, result) => {
-    let rawQuery = ``;
-    console.log(body);
-    if (body.isBookmark === "true") {
-      rawQuery = `DELETE FROM bookmark WHERE resident_r_id=? AND agentList_ra_regno=?`;
-    } else {
-      rawQuery = `INSERT INTO bookmark (resident_r_id, agentList_ra_regno) values (?, ?)`;
+    updateBookmark: async (id, body, result) => {
+		let rawQuery = ``;
+		console.log(body);
+		if (body.isBookmark === "true") {
+			rawQuery = `DELETE FROM bookmark WHERE resident_r_id=? AND agentList_ra_regno=?`;
+		} else {
+			rawQuery = `INSERT INTO bookmark (resident_r_id, agentList_ra_regno) values (?, ?)`;
+		}
+		try {
+			const res = await db.query(rawQuery, [id, body.raRegno]);
+			result(res);
+		} catch (error) {
+			result(null, error);
+		}
     }
-    try {
-      const res = await db.query(rawQuery, [id, body.raRegno]);
-      result(res);
-    } catch (error) {
-      result(null, res);
-    }
-  },
 };
