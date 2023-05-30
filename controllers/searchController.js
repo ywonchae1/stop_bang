5a1f96472a4de8881bb23f98f5f43ec75bc2dda9
ywonchae1 <ywonchae62@gmail.com>
2023-05-30 11:43:26 +0900

    #25 fix: 후기 없을 때 undefined 오류 수정

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -13,29 +13,29 @@ exports.getAgency = async(req,res) => {
     console.log(`get agencies // sgg_nm: ${sgg_nm}, bjdong_nm: ${bjdong_nm}`);
 
     try {
-        const rows = await searchModel.getAgenciesModel(sgg_nm, bjdong_nm);
-        res.json({ rows: rows });
+	const rows = await searchModel.getAgenciesModel(sgg_nm, bjdong_nm);
+	res.json({ rows: rows });
     } catch (err) {
-        console.error(err.stack)
+	console.error(err.stack)
     }
 };
 
 // 중개업소 정보 조회 API
 exports.getOneAgency = async(req, res) => {
-  
+
 
     const sgg_nm = req.query.sgg_nm;
     const bjdong_nm = req.query.bjdong_nm;
     const cmp_nm = '%'+req.query.cmp_nm+'%';
 
     try {
-            const rows = await searchModel.getOneAgencyModel(sgg_nm,bjdong_nm,cmp_nm);
-                 console.log('*****An agency info : ', rows);
-                 res.json({ rows: rows });
-        } catch (err) {
-        console.error(err.stack)
+	const rows = await searchModel.getOneAgencyModel(sgg_nm,bjdong_nm,cmp_nm);
+	console.log('*****An agency info : ', rows);
+	res.json({ rows: rows });
+    } catch (err) {
+	console.error(err.stack)
     }
-    
+
 
 };
 
