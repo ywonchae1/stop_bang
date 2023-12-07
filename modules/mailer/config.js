2f3911940ca18a1e6da9c488fda1c9018e2f9b70
MinYeongPark <yingo24655@naver.com>
2023-12-07 17:29:27 +0900

    [CHORE] index.js 파일 수정, configmap 적용

---
+++ modules/mailer/config.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+const config = {
+          user: process.env.MAILER_ID,
+          password: process.env.MAILER_PW,
+          host: process.env.MAILER_HOST,
+          port: process.env.MAILER_PORT,
+};
+
+module.exports = config;
