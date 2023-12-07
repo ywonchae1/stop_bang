68d0cbbe5f9deb34b11684574971fac6303e6b25
ywonchae1 <ywonchae62@gmail.com>
2023-12-07 18:00:20 +0900

    [FIX] jenkins env

--- modules/mailer/config.js
+++ modules/mailer/config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
 const config = {
-          user: process.env.MAILER_ID,
-          password: process.env.MAILER_PW,
-          host: process.env.MAILER_HOST,
-          port: process.env.MAILER_PORT,
+    user: process.env.MAILER_ID,
+    password: process.env.MAILER_PW,
+    host: process.env.MAILER_HOST,
+    port: process.env.MAILER_PORT,
 };
 
 module.exports = config;
